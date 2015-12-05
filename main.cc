63835c370ad71bc7707f5e49a10f6119b53827dd
THZ <thzhu@uwaterloo.ca>
2015-12-05 12:44:38 -0600

    added main tasks

--- main.cc
+++ main.cc
@@ -3,12 +3,22 @@
 #include "config.h"
 
 #include "Printer.h"
+#include "Bank.h"
+#include "Parent.h"
+#include "WATCardOffice.h"
+#include "NameServer.h"
+#include "VendingMachine.h"
+#include "BottlingPlant.h"
+#include "Student.h"
+#include "Groupoff.h"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 using namespace std;
 #include <cstdlib>	
+
+#include "debug.h"
 
 MPRNG rng;
 
@@ -42,14 +52,74 @@ void uMain::main() {
 		case 1:
 			break;
 		default:
-			// error
+			usage( argv );
 			break;
 	}
 
 	ConfigParms params;
 	processConfigFile(configFile.c_str(), params);
+
+	D(cout << "Config:" << endl << "sodaCost:" << params.sodaCost  << endl
+    		<< "numStudents: " << params.numStudents << endl
+    		<< "maxPurchases: " << params.maxPurchases << endl
+    		<< "numVendingMachines: " << params.numVendingMachines << endl
+    		<< "maxStockPerFlavour: " << params.maxStockPerFlavour << endl
+    		<< "maxShippedPerFlavour: " << params.maxShippedPerFlavour << endl
+    		<< "timeBetweenShipments: " << params.timeBetweenShipments << endl
+    		<< "groupoffDelay: " << params.groupoffDelay << endl
+    		<< "parentalDelay: " << params.parentalDelay << endl
+    		<< "numCouriers: " << params.numCouriers << endl;
+		);
+
 	rng.seed(seed);
 
-	// todo
+	// start tasks
+	Printer printer(params.numStudents, params.numVendingMachines, params.numCouriers);
+	Bank bank(params.numStudents);
+
+	Parent *parent = new Parent(printer, bank, params.numStudents, params.parentalDelay);
+
+	WATCardOffice *cardOffice = new WATCardOffice(printer, bank, params.numCouriers);
+
+	NameServer *nameServer = new NameServer(printer, params.numVendingMachines, params.numStudents);
+
+	VendingMachine *vendingMachines[params.numVendingMachines];
+
+	for (unsigned int i = 0; i < params.numVendingMachines; i++) {
+		vendingMachines[i] = new VendingMachine(printer, *nameServer, i, 
+												params.sodaCost, params.maxStockPerFlavour);
+	}
+
+	BottlingPlant *bottlingPlant = new BottlingPlant(printer, *nameServer, 
+													params.numVendingMachines, params.maxShippedPerFlavour, 
+													params.maxStockPerFlavour, params.timeBetweenShipments);
+
+	Groupoff *groupoff = new Groupoff(printer, params.numStudents, params.sodaCost, params.groupoffDelay);
 	
+	Student *students[params.numStudents];
+	for (unsigned int i = 0; i < params.numStudents; i++) {
+		students[i] = new Student(printer, *nameServer, *cardOffice, *groupoff, i, params.maxPurchases);
+	}
+
+
+
+	// finish
+	for (unsigned int i = 0; i < params.numStudents; i++) {
+		delete students[i];
+	}
+
+	delete groupoff;
+
+	delete bottlingPlant;
+
+	for (unsigned int i = 0; i < params.numVendingMachines; i++) {
+		delete vendingMachines[i];
+	}
+
+	delete nameServer;
+	delete cardOffice;
+	delete parent;
+
+
+
 } 
