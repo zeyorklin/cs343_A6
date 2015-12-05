687843208cfa76b0ed7f4d2b659b724c53ff881f
THZ <thzhu@uwaterloo.ca>
2015-12-04 20:09:59 -0600

    added student

--- Student.cc
+++ Student.cc
@@ -1,6 +1,10 @@
 #include "Student.h"
-#include "vendingMachine.h"
+#include "VendingMachine.h"
 #include "WATCard.h"
+#include "MPRNG.h"
+#include "Groupoff.h"
+#include "WATCardOffice.h"
+
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff, unsigned int id, unsigned int maxPurchases ) 
 				: prt(prt), nameServer(nameServer), cardOffice(cardOffice), groupoff(groupoff), id(id), maxPurchases(maxPurchases) {
@@ -10,6 +14,36 @@ Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffic
 
 
 void Student::main() {
+	purchase = rng(1, maxPurchases);
+	flavour = (VendingMachine::Flavours)rng((unsigned int)VendingMachine::Flavours.NUM_FLAVOUR -1);
 
+	prt.print(Printer::Student, 'S', flavour, purchase);
+
+	// create card
+	WATCard::FWATCard card = cardOffice.create(id, 5);
+	WATCard::FWATCard gift = groupoff.giftCard();
+
+	// get machine location
+	VendingMachine *machine = nameServer.getMachine(id);
+	prt.print(Printer::Student, 'V', machine->getId());
+
+
+
+	for (unsigned int purchased = 0; i < purchase; ) {
+		yield(rng(1, 10));
+		try {
+			machine->buy(flavour, gift.balance() > 0 : gift : card);
+			purchased++;
+			prt.print(Printer::Student, );
+		} catch (WATCardOffice::Lost &e) {
+
+		} catch (VendingMachine::Funds &e) {
+
+		} catch (VendingMachine::Stock &e) {
+
+		}
+	}
+
+	prt.print(Printer::Student, 'F');
 }
 
--- Student.h
+++ Student.h
@@ -6,6 +6,7 @@
 #include "WATCardOffice.h"
 #include "Groupoff.h"
 #include "MPRNG.h"
+#include "VendingMachine.h"
 
 _Task Student {
 	Printer &prt;
@@ -14,6 +15,10 @@ _Task Student {
 	Groupoff &groupoff;
 	unsigned int id;
 	unsigned int maxPurchases;
+
+	unsigned int purchase;
+	VendingMachine::Flavours flavour;
+
     void main();
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff,
